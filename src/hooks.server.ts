e6dc936cb9decb80514d031e5a672310a09f8d9c
Jamin <jaminwebdev@gmail.com>
2025-04-23 15:57:40 -0500

    pass keys explicitly

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,8 @@
 import { withClerkHandler } from 'svelte-clerk/server';
+import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
+import { CLERK_SECRET_KEY } from '$env/static/private';
 
-export const handle = withClerkHandler();
+export const handle = withClerkHandler({
+	publishableKey: PUBLIC_CLERK_PUBLISHABLE_KEY,
+	secretKey: CLERK_SECRET_KEY
+});
