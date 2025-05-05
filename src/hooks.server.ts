a3d5d44bcffbf4c71f6680a14a5eba6db2254079
Jamin <jaminwebdev@gmail.com>
2025-05-05 13:01:02 -0500

    add after login urls

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,5 +4,7 @@ import { CLERK_SECRET_KEY } from '$env/static/private';
 
 export const handle = withClerkHandler({
 	publishableKey: PUBLIC_CLERK_PUBLISHABLE_KEY,
-	secretKey: CLERK_SECRET_KEY
+	secretKey: CLERK_SECRET_KEY,
+	afterSignInUrl: '/dashboard',
+	afterSignUpUrl: '/dashboard'
 });
