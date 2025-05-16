54a3733d4c6cbe92fe623ad2e51ea0ca97be775c
Jamin <jaminwebdev@gmail.com>
2025-05-16 14:37:05 -0500

    pill subheading component

---
+++ src/lib/components/PillSubheading.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { cn } from '../utils';
+
+	type Props = {
+		children: Snippet;
+		classes?: string;
+	};
+
+	let { children, classes }: Props = $props();
+</script>
+
+<span
+	class={cn(
+		classes,
+		'bg-primary/20 text-primary rounded-full px-4 py-1 text-center text-sm font-semibold'
+	)}>{@render children()}</span
+>
