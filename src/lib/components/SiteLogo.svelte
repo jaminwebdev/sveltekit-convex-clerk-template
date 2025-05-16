904100a358e01cd9969680d0b4ffd9554d2bf817
Jamin <jaminwebdev@gmail.com>
2025-05-16 14:37:16 -0500

    site logo abstracted to component

---
+++ src/lib/components/SiteLogo.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Boxes } from 'lucide-svelte';
+</script>
+
+<a href="/" class="flex items-center gap-1">
+	<Boxes size="32" />
+	<span class="text-2xl font-black">Site Logo</span>
+</a>
