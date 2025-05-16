c6bf323d4ddbee34b98861e12f3bbb410b15ca54
Jamin <jaminwebdev@gmail.com>
2025-05-16 15:46:19 -0500

    marketing deskto nav

--- src/lib/components/marketing/navigation/MarketingDesktopNav.svelte
+++ src/lib/components/marketing/navigation/MarketingDesktopNav.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
 	import { SignedIn, UserButton } from 'svelte-clerk/client';
 	import ThemeToggle from '@/lib/components/ThemeToggle.svelte';
-	import { Boxes } from '@lucide/svelte';
 	import Container from '@/lib/components/Container.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { LayoutDashboard } from 'lucide-svelte';
+	import SiteLogo from '@/lib/components/SiteLogo.svelte';
 </script>
 
 <div class="border-b-primary/50 border-1">
 	<Container classes="max-w-[88rem]">
 		<header class=" flex justify-between p-5">
-			<a href="/" class="flex items-center gap-1">
-				<Boxes size="32" />
-				<span class="text-2xl font-black">Site Logo</span>
-			</a>
+			<SiteLogo />
 			<nav class="hidden gap-12 lg:flex 2xl:ml-16">
 				<a href="/#" class="hover:text-primary text-lg font-semibold transition duration-100"
 					>Collections</a
