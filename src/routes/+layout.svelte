22f2731923eb24788a6cb1bae66846b13ae0ac3b
Jamin <jaminwebdev@gmail.com>
2025-07-28 15:41:39 -0500

    rename themestore file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	import { ClerkProvider } from 'svelte-clerk';
 	import { Toaster } from '@/lib/components/ui/sonner/index.js';
 	import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
-	import { useTheme } from '@/stores/ThemeStove.svelte';
+	import { useTheme } from '@/stores/ThemeStore.svelte';
 	import { setThemeContext } from '@/contexts/ThemeContext';
 
 	setThemeContext(useTheme);
